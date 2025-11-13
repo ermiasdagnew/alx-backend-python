76f6984217d84f0bc3f95aa72a4e5549f49aa797
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-13 21:44:17 +0300

    Add files via upload

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,24 +1,7 @@
-#!/usr/bin/env python3
-"""Unit tests for utils.access_nested_map function."""
-
-import unittest
-from parameterized import parameterized
-from utils import access_nested_map
-
-
-class TestAccessNestedMap(unittest.TestCase):
-    """Test cases for access_nested_map."""
-
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
-    def test_access_nested_map(self, nested_map, path):
-        """Test that KeyError is raised with proper message."""
-        with self.assertRaises(KeyError) as cm:
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
-        self.assertEqual(str(cm.exception), f"'{path[-1]}'")
-
-
-if __name__ == "__main__":
-    unittest.main()
