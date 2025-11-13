0faece047772ef5495df4be08f3898f8f811d4a7
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-13 22:03:27 +0300

    Delete 0x03-Unittests_and_integration_tests directory

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++
@@ -1,7 +0,0 @@
-    @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
-    ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        with self.assertRaises(KeyError):
-            access_nested_map(nested_map, path)
