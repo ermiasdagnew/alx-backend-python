e210a2b3bb45f9e32bc3a50fc26aad525849e62a
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-13 12:49:17 +0300

    Add files via upload

---
+++ messaging_app/chats/urls.py
@@ -0,0 +1,15 @@
+4. Set up url routing
+mandatory
+Objective: configure URLS for the conversations and messages
+
+Instructions:
+
+Using Django rest framework DefaultRouter to automatically create the conversations and messages for your viewsets
+
+Navigate to the main project’s urls.py i.e messaging_app/urls.py and include your created routes with path as api
+
+Repo:
+
+GitHub repository: alx-backend-python
+Directory: messaging_app
+File: messaging_app/chats/urls.py
