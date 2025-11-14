f41a2eae40c90216f17545fba7b6af68cc38168f
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-14 09:52:51 +0300

    Add files via upload

---
+++ messaging_app/chats/views.py
@@ -0,0 +1,38 @@
+from rest_framework import viewsets, filters
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import status
+
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    # Adding filters
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['participants__first_name', 'participants__last_name']
+    ordering_fields = ['created_at']
+
+    @action(detail=True, methods=['post'])
+    def send_message(self, request, pk=None):
+        """
+        Endpoint to send a message in an existing conversation
+        URL: /conversations/<conversation_id>/send_message/
+        """
+        conversation = self.get_object()
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(conversation=conversation, sender=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['message_body', 'sender__first_name', 'sender__last_name']
+    ordering_fields = ['sent_at']
