5f199bfad83fca1ba09dc490b6cdc2457f748286
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-17 15:22:37 +0300

    Add files via upload

---
+++ messaging_app/chats/views.py
@@ -0,0 +1,20 @@
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+from .permissions import IsParticipantOfConversation
+from .pagination import MessagePagination
+from .filters import MessageFilter
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsParticipantOfConversation]
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsParticipantOfConversation]
+    pagination_class = MessagePagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = MessageFilter
