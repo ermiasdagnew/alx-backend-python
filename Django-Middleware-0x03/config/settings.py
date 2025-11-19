04ea24ee8c1b127cdabb2214f2437e1d244f192b
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-19 16:17:40 +0300

    Update settings.py

--- Django-Middleware-0x03/config/settings.py
+++ Django-Middleware-0x03/config/settings.py
@@ -1,1 +1,41 @@
-# settings.py placeholder with MIDDLEWARE configured
+# Django-Middleware-0x03/config/settings.py
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'chats',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Custom middleware
+    'chats.middleware.RequestLoggingMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+SECRET_KEY = 'django-insecure-test-key'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+
+STATIC_URL = '/static/'
