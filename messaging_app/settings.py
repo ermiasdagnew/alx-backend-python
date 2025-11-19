0ac0e145d03200732c16ab0d7ec42baf2b121ace
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-19 17:53:15 +0300

    Update settings.py

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -110,3 +110,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
 }
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
