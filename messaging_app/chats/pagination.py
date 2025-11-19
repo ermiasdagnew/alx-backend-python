b6215c9bd2eb6d2834d37dc54dcb03a1d223b084
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-19 18:00:50 +0300

    Update pagination.py

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,6 +1,10 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class MessagePagination(PageNumberPagination):
     page_size = 20
-    page_size_query_param = 'page_size'
-    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'total_messages': self.page.paginator.count,
+            'messages': data
+        })
