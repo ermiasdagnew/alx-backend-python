52228cd973a4c9455d79e0dd901ee324ad0262eb
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-19 16:13:48 +0300

    Update middleware.py

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,1 +1,24 @@
-# middleware.py placeholder with all middleware classes
+# chats/middleware.py
+from datetime import datetime
+import logging
+
+# Configure a logger to write to a file
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('requests.log')
+formatter = logging.Formatter('%(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = getattr(request, 'user', 'Anonymous')
+        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
+        logger.info(log_message)
+
+        response = self.get_response(request)
+        return response
