913b20052a62a8ae9282499b39bba7d840903756
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-20 12:59:49 +0300

    Update settings.py

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -1,3 +1,11 @@
+INSTALLED_APPS = [
+INSTALLED_APPS = [
+    ...
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'django_filters',
+    'chats',   # your application
+]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -6,10 +14,12 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
-from datetime import timedelta
-
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-}
-ls -R
+REST_FRAMEWORK.update({
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+})
+REST_FRAMEWORK.update({
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+})
