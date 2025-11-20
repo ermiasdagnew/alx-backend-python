ed5d400fd1b0f56819b8d5ebba9d3f841a43fbea
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-20 16:43:49 +0300

    Update settings.py

--- Django-Middleware-0x03/ Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/ Django-Middleware-0x03/settings.py
@@ -10,3 +10,18 @@ INSTALLED_APPS = [
     'apps.chats',
     'apps.users',
 ]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Custom middleware
+    'apps.chats.middleware.RequestLoggingMiddleware',
+    'apps.chats.middleware.RestrictAccessByTimeMiddleware',
+    'apps.chats.middleware.OffensiveLanguageMiddleware',
+    'apps.chats.middleware.RolepermissionMiddleware',
+]
