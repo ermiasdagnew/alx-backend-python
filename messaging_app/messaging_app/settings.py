6ac0e7e67aff149ad410b0b047a40de37104e235
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-22 13:44:22 +0300

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -1,40 +1,5 @@
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    # Third-party apps
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'django_filters',
-
-    # Your apps
-    'chats',
+MIDDLEWARE = [
+    ...
+    'chats.middleware.RequestLoggingMiddleware',
+    ...
 ]
-
-# Django REST Framework configuration
-REST_FRAMEWORK = {
-    # Authentication classes: Basic, Session, JWT
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-
-    # Permission: only authenticated users
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-
-    # Pagination settings
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
-
-    # Filtering backend
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ],
-}
