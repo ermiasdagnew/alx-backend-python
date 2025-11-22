8970c1f7e55b59df29667b0969296cd7a42f1350
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-22 13:46:00 +0300

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -1,5 +1,38 @@
-MIDDLEWARE = [
-    ...
-    'chats.middleware.RequestLoggingMiddleware',
-    ...
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-change-me-task4'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+'django.contrib.admin',
+'django.contrib.auth',
+'django.contrib.contenttypes',
+'django.contrib.sessions',
+'django.contrib.messages',
+'django.contrib.staticfiles',
+'chats',
 ]
+
+MIDDLEWARE = [
+'django.middleware.security.SecurityMiddleware',
+'django.contrib.sessions.middleware.SessionMiddleware',
+'django.middleware.common.CommonMiddleware',
+
+'chats.middleware.RolePermissionMiddleware', # TASK 4 ONLY
+
+'django.middleware.csrf.CsrfViewMiddleware',
+'django.contrib.auth.middleware.AuthenticationMiddleware',
+'django.contrib.messages.middleware.MessageMiddleware',
+'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'messaging_app.urls'
+WSGI_APPLICATION = 'messaging_app.wsgi.application'
+
+DATABASES = {
+'default': {
+'ENGINE': 'django.db.backends.sqlite3',
+'NAME': BASE_DIR / 'db.sqlite3
