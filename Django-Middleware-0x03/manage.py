bda97165468adbe977efe8a5a453ec466751f0ea
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-22 13:11:09 +0300

    Update manage.py

--- Django-Middleware-0x03/manage.py
+++ Django-Middleware-0x03/manage.py
@@ -1,1 +1,13 @@
-# Placeholder for manage.py
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == '__main__':
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messaging_app.settings')
+try:
+from django.core.management import execute_from_command_line
+except ImportError as exc:
+raise ImportError(
+"Couldn't import Django. Are you sure it's installed and available on your PYTHONPATH?"
+) from exc
+execute_from_command_line(sys.argv)
