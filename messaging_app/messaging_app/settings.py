69fa2f89fb8b8e355c1f87b81a2686face03efa6
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-23 17:08:52 +0300

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -27,3 +27,19 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',  # only logged-in users can access API
     ),
 }
+INSTALLED_APPS = [
+    # Default Django apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Third-party
+    'rest_framework',
+    'django_filters',
+
+    # Your app
+    'chats',
+]
