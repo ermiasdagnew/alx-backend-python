5c49ed2a8f4b89a15f18e61b7528d63ce5458549
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-23 16:44:41 +0300

    Update middleware.py

--- messaging_app/chats/middleware.py
+++ messaging_app/chats/middleware.py
@@ -1,22 +1,24 @@
 from datetime import datetime
+import logging
+
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('requests.log')
+formatter = logging.Formatter('%(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
 
 class RequestLoggingMiddleware:
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        print(f"{datetime.now()} - Path: {request.path}")
-        return self.get_response(request)
+        user = request.user if request.user.is_authenticated else "Anonymous"
 
-
-class RolepermissionMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        # Example: Only authenticated users allowed
-        if not request.user.is_authenticated:
-            from django.http import HttpResponseForbidden
-            return HttpResponseForbidden("Not allowed")
+        logger.info(
+            f"{datetime.now()} - User: {user} - Path: {request.path}"
+        )
 
         return self.get_response(request)
