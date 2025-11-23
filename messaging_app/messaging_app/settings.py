5f61eb20a0847277a0c92c80557ed3f108d65457
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-23 16:36:24 +0300

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -1,31 +1,48 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-change-me-task4'
+SECRET_KEY = 'replace-this-with-any-string'
 DEBUG = True
+
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-'django.contrib.admin',
-'django.contrib.auth',
-'django.contrib.contenttypes',
-'django.contrib.sessions',
-'django.contrib.messages',
-'django.contrib.staticfiles',
-'chats',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Your apps
+    'chats',
 ]
 
 MIDDLEWARE = [
-   'chats.middleware.RequestLoggingMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
+    # Your custom middlewares
+    'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RolepermissionMiddleware',
 ]
 
-
 ROOT_URLCONF = 'messaging_app.urls'
+
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.sqlite3',
-'NAME': BASE_DIR / 'db.sqlite3
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
+
+STATIC_URL = 'static/'
