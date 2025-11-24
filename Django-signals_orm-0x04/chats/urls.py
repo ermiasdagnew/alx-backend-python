8a0a702581577a7a4122998a8709a2ffa64a4ad7
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-24 10:21:24 +0300

    Update urls.py

--- Django-signals_orm-0x04/chats/urls.py
+++ Django-signals_orm-0x04/chats/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import message_list, delete_user
 
 urlpatterns = [
-    path('', views.message_list, name='message_list'),
+    path('', message_list, name='message_list'),
+    path('delete/', delete_user, name='delete_user'),
 ]
